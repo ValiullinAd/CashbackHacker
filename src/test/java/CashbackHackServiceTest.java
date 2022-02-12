72d2037d760226186db3b140893965265d73c721
ValiullinAd <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>
2022-02-12 20:04:56 +0300

    Initial commit

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+}
